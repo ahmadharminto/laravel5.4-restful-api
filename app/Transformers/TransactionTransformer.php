785058992ad134a60494c2c66dfa6f0b90fdcc9b
Ahmad Harminto <harminto_ahmad@live.com>
2017-09-21 17:00:59 +0700

    implementation : HATEOAS

--- app/Transformers/TransactionTransformer.php
+++ app/Transformers/TransactionTransformer.php
@@ -18,10 +18,32 @@ class TransactionTransformer extends TransformerAbstract
             'identifier' => (int)$transaction->id,
             'quantity' => (int)$transaction->quantity,
             'buyer' => (int)$transaction->buyer_id,
-            'product' => (int)$transaction->product_id,
+            'transaction' => (int)$transaction->transaction_id,
             'creationDate' => (string)$transaction->created_at,
             'lastChange' => (string)$transaction->updated_at,
             'deletedDate' => isset($transaction->deleted_at) ? (string)$transaction->deleted_at : null,
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('transactions.show', $transaction->id)
+                ],
+                [
+                    'rel' => 'transaction.categories',
+                    'href' => route('transactions.categories.index', $transaction->id)
+                ],
+                [
+                    'rel' => 'transaction.seller',
+                    'href' => route('transactions.sellers.index', $transaction->id)
+                ],
+                [
+                    'rel' => 'buyer',
+                    'href' => route('buyers.show', $transaction->buyer_id)
+                ],
+                [
+                    'rel' => 'product',
+                    'href' => route('products.show', $transaction->product_id)
+                ]
+            ]
         ];
     }
 
@@ -31,7 +53,7 @@ class TransactionTransformer extends TransformerAbstract
             'identifier' => 'id',
             'quantity' => 'quantity',
             'buyer' => 'buyer_id',
-            'product' => 'product_id',
+            'transaction' => 'transaction_id',
             'creationDate' => 'created_at',
             'lastChange' => 'updated_at',
             'deletedDate' => 'deleted_at',
